6fddd44614e31aabe2b6c16f33cef2919906d906
Felix-Olivier Landry <felixo2997@gmail.com>
2019-10-29 11:51:19 -0400

    Connections to rooms done

--- clients/mobile/js/controller.js
+++ clients/mobile/js/controller.js
@@ -1,16 +1,14 @@
 window.addEventListener("load", () => {
 
     let socket = io.connect();
-    let connectButton = document.querySelector("#connect");
-
+    
     let username = "";
     let roomNumber = 0;
     let team = "";
     let id = 0;
     
     //Connect to a room
-
-    
+    let connectButton = document.querySelector("#connect");
     connectButton.onclick = e => {
         e.preventDefault();
 
@@ -23,30 +21,35 @@ window.addEventListener("load", () => {
                 teamInput = radio[i].value;
             }
         }
-    
-        socket.emit('roomConnect', userInput, roomInput, teamInput, function(data, idPlayer){
-            if(data === "roomValid"){
-                username = userInput;
-                roomNumber = roomInput;
-                team = teamInput;
-                id = idPlayer;
 
-                document.querySelector("#login-screen").style.display = "none";
-                document.querySelector("#controller").style.display = "grid";
-            }
-            else if(data === "roomFull"){
-                alert("This room is full.");
-            }
-            else if(data === "roomNull"){
-                alert("This room doesn't exist.");
-            }
-            else if(data === "teamFull"){
-                alert("The team you selected is full.");
-            }
-        });
+        if(userInput == null || roomInput == null || teamInput == ""){
+            alert("Missing informations.")
+        }
+        else{
+            socket.emit('roomConnect', userInput, roomInput, teamInput, function(data, idPlayer){
+                if(data === "roomValid"){
+                    username = userInput;
+                    roomNumber = roomInput;
+                    team = teamInput;
+                    id = idPlayer;
+    
+                    document.querySelector("#login-screen").style.display = "none";
+                    document.querySelector("#controller").style.display = "grid";
+                }
+                else if(data === "roomFull"){
+                    alert("This room is full.");
+                }
+                else if(data === "roomNull"){
+                    alert("This room doesn't exist.");
+                }
+                else if(data === "teamFull"){
+                    alert("The team you selected is full.");
+                }
+            });
+        }
+    
     }
 
-    
     //Controller
 
 });
