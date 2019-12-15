bb09a122bae25718b1a42112d52c01ae313ce2c4
Felix-Olivier Landry <felixo2997@gmail.com>
2019-12-15 01:32:50 -0500

    test

--- clients/mobile/js/controller.js
+++ clients/mobile/js/controller.js
@@ -72,47 +72,57 @@ window.addEventListener("load", () => {
 
     //Up events
     up.ontouchstart = e =>{
+        e.preventDefault();
         socket.emit('up-on');    
     }
 
     up.ontouchend = e =>{
+        e.preventDefault();
         socket.emit('up-off');
     }
 
     //Right events
     right.ontouchstart = e =>{
+        e.preventDefault();
         socket.emit('right-on');       
     }
 
     right.ontouchend = e =>{
+        e.preventDefault();
         socket.emit('right-off');
     }
 
     //Down events
     down.ontouchstart = e =>{
+        e.preventDefault();
         socket.emit('down-on');    
     }
 
     down.ontouchend = e =>{
+        e.preventDefault();
         socket.emit('down-off');
     }
 
     //Left events
     left.ontouchstart = e =>{
+        e.preventDefault();
         socket.emit('left-on');    
     }
 
     left.ontouchend = e =>{
+        e.preventDefault();
         socket.emit('left-off');
     }
 
     //Action a
-    actionA.ontouchend = e =>{    
+    actionA.ontouchend = e =>{
+        e.preventDefault();    
         socket.emit('action-a');    
     }
 
     //Action b
     actionB.ontouchend = e =>{
+        e.preventDefault();
         socket.emit('action-b');  
     }
 
